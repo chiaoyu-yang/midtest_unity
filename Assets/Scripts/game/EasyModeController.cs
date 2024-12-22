80495ec9c8c7d91f78e1194fd39f311f35d2d195
yehch <10856004@ntub.edu.tw>
2024-12-23 01:59:52 +0800

    修改-Level卡片數

--- Assets/Scripts/game/EasyModeController.cs
+++ Assets/Scripts/game/EasyModeController.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class EasyModeController : CardGameController
 {
     public override int GridRows => 2;
-    public override int GridCols => 2;
-    public override int[] Numbers => new int[] { 0, 0, 1, 1 };
+    public override int GridCols => 4;
+    public override int[] Numbers => new int[] { 0, 0, 1, 1, 2, 2, 3, 3 };
 }
--- Assets/Scripts/game/HardModeController.cs
+++ Assets/Scripts/game/HardModeController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class HardModeController : CardGameController
 {
-    public override int GridRows => 2;
+    public override int GridRows => 4;
     public override int GridCols => 4;
-    public override int[] Numbers => new int[] { 0, 0, 1, 1, 2, 2, 3, 3 };
+    public override int[] Numbers => new int[] { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
 }
